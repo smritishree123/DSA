45eadb5c59dbcd9e09d92111b5041a50d18277fd
smriti shree <smriti2199@gmail.com>
2021-09-13 21:45:06 +0530

    circle perimeter

---
+++ PerimeterCircle.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+ class PerimeterCircle {
+     public static void main(String[] args) { 
+        double radius; 
+       Scanner sc=new Scanner(System.in);   
+               // input radius of circle    
+    System.out.print("Enter the Radius of Circle : ");  
+      radius=sc.nextDouble();   
+               // circle parameter is 2 * pie * radius
+         double Perimeter=2*3.14*radius; 
+                System.out.print("Perimeter of Circle : " + Perimeter);  
+
+          } 
+}
---
+++ SwapTwoNumbers.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class SwapTwoNumbers 
+{
+    public static void main(String[] s)
+ {        int a, b;   
+     Scanner sc = new Scanner(System.in);
+     System.out.print("Enter value of a: ");  
+       a = sc.nextInt();   
+     System.out.print("Enter value of a: "); 
+       b = sc.nextInt();  
+     System.out.println("Before swapping - a: " + a + ", b: " + b);   
+       int temp;   
+     temp = a;   
+        a = b;   
+        b = temp;  
+      System.out.println("After swapping  - a: " + a + ", b: " + b);
+    }
+}
