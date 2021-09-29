c8e54fee1ec66a19371ec06cb1a65b740a9ed073
smriti shree <smriti2199@gmail.com>
2021-09-29 19:31:15 +0530

    CI

---
+++ Compound_Interest.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Compound_Interest {
+    /**     * @param args     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub 
+       double Principal_Amount = 0.0;
+        double Interest_Rate = 0.0;
+        double Time_Period = 0.0;
+        double CompoundInterest = 0.0;
+        Scanner i = new Scanner(System.in);   
+     System.out.print("Enter the Principal Amount : ");
+        Principal_Amount = i.nextDouble();
+        System.out.print("Enter the Time Period : ");  
+      Time_Period = i.nextDouble();
+        System.out.print("Enter the Interest Rate : ");
+        Interest_Rate = i.nextDouble();
+        CompoundInterest = Principal_Amount * Math.pow((1 + Interest_Rate / 100), Time_Period);
+        System.out.println("");
+        System.out.println("Compound Interest : " + CompoundInterest);  
+  }
+}
